1177c675032b0383b66273c29f3f9859181aad76
Craig M. Stimmel <cstimmel@uw.edu>
2017-11-28 13:03:05 -0800

    Try collecting staticfiles to the ondeck location (with ev saved config v1)

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ec2-user/eb-virt/ebdjango/ebsrc/static'
+#STATIC_ROOT = '/home/ec2-user/eb-virt/ebdjango/ebsrc/static'
+STATIC_ROOT = '/opt/python/ondeck/app/static/'
 
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
