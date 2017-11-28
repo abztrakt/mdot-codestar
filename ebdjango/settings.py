0237de9f94804eedb1ef8f7dc2042349d649b7bb
Craig M. Stimmel <cstimmel@uw.edu>
2017-11-28 12:33:26 -0800

    Back to original static path.

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -128,8 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = '/home/ec2-user/eb-virt/ebdjango/ebsrc/static'
-STATIC_ROOT = '/opt/python/current/app/static'
+STATIC_ROOT = '/home/ec2-user/eb-virt/ebdjango/ebsrc/static'
 
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
